2721543c2ea1de7b918bf1fdeaf39a03314df330
Suzumiya894 <yueerwen@yeah.net>
2022-06-05 16:50:06 +0800

    add some debug sentence

--- backgroundFilter.py
+++ backgroundFilter.py
@@ -2,8 +2,9 @@ import pdb
 import cv2
 import numpy as np
 from PIL import Image,ImageStat
+from key_detection import key_detection_visual
 
-def moveTowards_filter(bgr,frm,step=0):
+def moveTowards_filter(bgr,frm,step=20):
     # pdb.set_trace()
     bgr = np.array(bgr,dtype=np.int16)
     frm = np.array(frm,dtype=np.int16)
@@ -19,19 +20,29 @@ def moveTowards_filter(bgr,frm,step=0):
 
 if __name__ == '__main__':
     background = "testFigures/219.bmp"
-    frame = "testFigures/303.bmp"
+    frame = "testFigures/268.bmp"
     from cropFigure import crop_fig
     bgr = cv2.imread(background, cv2.IMREAD_COLOR)
-    bgr,_ = crop_fig(bgr)
+    bgr,_,_ = crop_fig(bgr)
+    #key_detection_visual(bgr)
     frm = cv2.imread(frame, cv2.IMREAD_COLOR)
-    frm,_ = crop_fig(frm)
-    res = moveTowards_filter(bgr,frm)
+    frm,_,_ = crop_fig(frm)
     bgr = np.array(cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY))
-    res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    frm = np.array(cv2.cvtColor(frm, cv2.COLOR_BGR2GRAY))
+    #res = moveTowards_filter(bgr,frm)
+    res_black = moveTowards_filter(bgr,frm, 30)
+    res_white = moveTowards_filter(bgr,frm, 50)
     
+    #res = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+    print(np.shape(bgr))
+    print(np.shape(res_white))
 
-    white = np.clip(bgr.astype(np.int16)-res.astype(np.int16),a_min=0,a_max=255).astype(np.uint8)
-    black = np.clip(res.astype(np.int16)-bgr.astype(np.int16),a_min=0,a_max=255).astype(np.uint8)
+    white = np.clip(bgr.astype(np.int16)-res_white.astype(np.int16),a_min=0,a_max=1).astype(np.uint8)*255
+    black = np.clip(res_black.astype(np.int16)-bgr.astype(np.int16),a_min=0,a_max=1).astype(np.uint8)*255
+    cv2.imshow("result_img", white )
+    cv2.waitKey(0)
+    cv2.imshow("result_img", black )
+    cv2.waitKey(0)
     _, thresh_white = cv2.threshold(white,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     _, thresh_black = cv2.threshold(black,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     # _, thresh = cv2.threshold(src=black, thresh=30, maxval=255, type=0)
