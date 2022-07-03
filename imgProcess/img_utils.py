ce1ea19ce97748941ea374006d01cab53e880981
Suzumiya894 <yueerwen@yeah.net>
2022-07-03 13:44:27 +0800

    fix a bug

--- imgProcess/img_utils.py
+++ imgProcess/img_utils.py
@@ -8,6 +8,6 @@ def savefig_keyboard(current_pwd, path,fig,overwrite):
     if overwrite and os.path.isfile(fig_path):
         os.remove(fig_path)
     if current_pwd.split("\\")[-2] == "video_106":
-        fig_crop = fig[0:64, :]
-    cv.imwrite(fig_path,fig_crop)
+        fig = fig[0:64, :]
+    cv.imwrite(fig_path,fig)
     print("write {}".format(path))
