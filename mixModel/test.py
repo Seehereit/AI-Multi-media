129576a3aedd0044adacc1ebdfdd3df85828df1a
Suzumiya894 <yueerwen@yeah.net>
2022-07-03 14:51:44 +0800

    dataset update

--- mixModel/test.py
+++ mixModel/test.py
@@ -1,22 +1,21 @@
-import cv2
+import cv2, os, re
 import numpy as np
-fig1 = np.ones((64, 320))
-fig1 = fig1 * 255
-cv2.imshow("fig1", fig1)
-cv2.waitKey(0)
-path = "mixModel/data/SIGHT/image/video_100/mix"
-test1 = cv2.imread(path + "/4900.bmp", cv2.IMREAD_GRAYSCALE)
-test2 = cv2.resize(test1, (320, 64))
-print(np.shape(test2))  
-cv2.imshow("test2", test2)
-cv2.waitKey(0)
-test1 = cv2.resize(test1, (64, 320))
-print(np.shape(test1))
-cv2.imshow("test1", test1)
-cv2.waitKey(0)
-#cv2.imwrite('.', fig1)
-fig2 = np.ones((320, 64))
-fig2 = fig2 * 255
-cv2.imshow("fig2", fig2)
-cv2.waitKey(0)
+import soundfile, json
+data = {1:"sss"}
+test1 = {}
+test1[1] = data
+print(test1)
+data = {2:"bbb"}
+print(test1)
+path = sorted(os.listdir("mixModel/data/SIGHT\\image\\video_131\\" + "testFigures_keyboard"),key = lambda i:int(re.match(r'(\d+)',i).group()))[0]
+print(path)
+print("mixModel/data/SIGHT\\flac\\video_131\\"[0:-1] + ".flac")
+audio, sr = soundfile.read("mixModel/data/SIGHT\\flac\\video_131\\"[0:-1] + ".flac", dtype='int16')
+print(np.shape(audio))
+print(np.sum(audio == 0))
+
+with open("mixModel/data/SIGHT\\image\\video_131" + "\\dataset_config.json") as f:
+    dataset_config = json.load(f)
+for e in dataset_config:
+    print(e)
 #cv2.imwrite('.', fig2)
